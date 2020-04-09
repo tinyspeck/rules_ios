c2fdf24d197191163ae654ab7ad4e44e997cb5d9
Wiley Kestner <wkestner@slack-corp.com>
2020-04-09 12:46:42 -0400

    Add Objective-C source files to Mixed-Source-Example
    - Makes the App target contain both Objective-C and Swift files
    
    - Temporarily removes the MixedSourceFramework import issue so we can ensure the App builds without errors
    
    - App target can be built with `bazel build tests/mixed-source-app/ios:App`

---
+++ tests/mixed-source-app/ios/App/Source/ViewController.m
@@ -0,0 +1,11 @@
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    [self.view setBackgroundColor:[UIColor greenColor]];
+}
+
+@end
