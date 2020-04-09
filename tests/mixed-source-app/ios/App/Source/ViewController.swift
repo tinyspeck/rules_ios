ad7e9a2807db31ca6064e506a4eb08ac5abc3695
Jonathan Schear <jschear@slack-corp.com>
2020-04-09 11:29:56 -0400

    Add example app in Swift

---
+++ tests/mixed-source-app/ios/App/Source/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Jonathan Schear on 4/8/20.
+//  Copyright © 2020 Jonathan Schear. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
